1015430c9515277c4f8ee128705b44a09678cb43
GracieKYS <117329856+GracieKYS@users.noreply.github.com>
2023-01-12 11:30:27 +0800

    Add files via upload

---
+++ project/script.js
@@ -0,0 +1,25 @@
+const text = document.querySelector('.container-text');
+
+function mouseMoveFunction(event) {
+
+    let currentXPercentage = event.clientX / window.innerWidth;
+    let currentYPercentage = event.clientY / window.innerHeight;
+
+    text.style.top = -(currentYPercentage * 100) + 100 + '%';
+    text.style.left = -(currentXPercentage * 100) + 100 + '%';
+    console.log(-(currentYPercentage * 100) + 100);
+    if(-(currentXPercentage * 100) + 100 < 15) {
+        text.style.left = 15 + "%";
+    }
+    if(-(currentXPercentage * 100) + 100 > 85) {
+        text.style.left = 85 + "%";
+    }
+    if(-(currentYPercentage * 100) + 100 < 10) {
+        text.style.top = 10 + "%";
+    }
+    if(-(currentYPercentage * 100) + 100 > 85) {
+        text.style.top = 85 + "%";
+    }
+}
+
+window.addEventListener('mousemove', mouseMoveFunction);
